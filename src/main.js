c4f5e5ac0d7a63b7eb977cb42663c1bde57ff64f
TomasNA <tna123yo@gmail.com>
2022-06-12 22:37:04 -0500

    dinamic categories

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ function create_element(element, clas_list=""){
 }
 
 async function getTrendingMoviesPreview(){
-    const res = await fetch("https:api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
+    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
     const data = await res.json();
 
-    const movies = data.results;
+    const res_categories = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+API_KEY);
+    const data_categories = await res_categories.json();
+    const categories = data_categories.genres;
 
+    const movies = data.results;
+    console.log(movies)
     const movies_grid = document.getElementById("movies-grid--popular");
     movies.forEach(movie => {
         const poster = create_element("img", "movie-container--poster");
@@ -28,12 +32,35 @@ async function getTrendingMoviesPreview(){
         movie_info.appendChild(date)
         movie_info.appendChild(calification)
 
+        const categories_container = create_element("div", "movie-info--categories-container");
+        movie.genre_ids.forEach(genre => {
+            const pill = create_element("div", "movie-info--categories-pill pill-"+genre+"-bg");
+            pill.innerText = categories.filter(category => category.id === genre)[0].name;
+            categories_container.appendChild(pill)
+        });
+
         const movie_container = create_element("article", "movie_container");
         movie_container.appendChild(poster);
         movie_container.appendChild(movie_info);
+        movie_container.appendChild(categories_container);
 
         movies_grid.appendChild(movie_container);
     });
 }
 
+async function getCategoriesPreview(){
+    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+
+    const categories_container = document.getElementById("categories-container");
+    categories.forEach(category => {
+        const pillCategory = create_element("div", "categories-pill pill-"+category.id+"-bg");
+        pillCategory.innerText = category.name;
+        categories_container.appendChild(pillCategory);
+    });
+}
+
 getTrendingMoviesPreview()
+getCategoriesPreview()
